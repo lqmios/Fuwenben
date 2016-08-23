513f69ff56a5ca73654e185e6468392a9feda556
lqm <lqm@lqmdemacbook-pro.local>
2016-08-23 12:03:20 +0800

    Initial Commit

---
+++ Fuwenben/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fuwenben
+//
+//  Created by lqm on 16/8/23.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
